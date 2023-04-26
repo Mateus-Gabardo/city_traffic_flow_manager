8e9c759a22d27a1d2e2cddf082061c0c857789bb
Mateus-Gabardo <92892326+Mateus-Gabardo@users.noreply.github.com>
2023-04-25 22:57:11 -0300

    atribuido parametro adicional no json

--- src/generator.py
+++ src/generator.py
@@ -3,11 +3,11 @@ from grafo_generator import GrafoJsonWriter
 
 grafo = GrafoJsonWriter()
 
-grafo.adicionar_aresta("A", "B", {"lenght": 10, "maxSpeed": 60, "numLanes" : 1})
-grafo.adicionar_aresta("B", "C", {"lenght": 8, "maxSpeed": 50, "numLanes" : 2})
-grafo.adicionar_aresta("C", "D", {"lenght": 12, "maxSpeed": 40, "numLanes" : 2})
-grafo.adicionar_aresta("D", "E", {"lenght": 15, "maxSpeed": 30, "numLanes" : 1})
-grafo.adicionar_aresta("E", "A", {"lenght": 5, "maxSpeed": 20, "numLanes" : 2})
+grafo.adicionar_aresta("A", "B", {"lenght": 10, "maxSpeed": 60, "numLanes" : 1, "priority": 100})
+grafo.adicionar_aresta("B", "C", {"lenght": 8, "maxSpeed": 50, "numLanes" : 2, "priority": 100})
+grafo.adicionar_aresta("C", "D", {"lenght": 12, "maxSpeed": 40, "numLanes" : 2, "priority": 100})
+grafo.adicionar_aresta("D", "E", {"lenght": 15, "maxSpeed": 30, "numLanes" : 1, "priority": 100})
+grafo.adicionar_aresta("E", "A", {"lenght": 5, "maxSpeed": 20, "numLanes" : 2, "priority": 100})
 
 nome_arquivo = "grafo.json"
 pasta_destino = "instances"
--- src/sumo_xml_generator.py
+++ src/sumo_xml_generator.py
@@ -11,7 +11,7 @@ class SumoFilesGenerator:
             f.write('<?xml version="1.0" encoding="UTF-8"?>\n')
             f.write('<nodes xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://sumo.dlr.de/xsd/nodes_file.xsd">\n')
             for i, v in enumerate(self.graph['vertices']):
-                f.write(f'   <node id="{i}" x="0.0" y="0.0" type="priority"/>\n')
+                f.write(f'   <node id="{v}" x="0.0" y="0.0" type="priority"/>\n')
             f.write('</nodes>')
 
     def generate_edges_file(self, filename, desteny):
@@ -21,8 +21,7 @@ class SumoFilesGenerator:
             f.write('<edges xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://sumo.dlr.de/xsd/edges_file.xsd">\n')
             for k, v in self.graph['arestas'].items():
                 src, dst = k.split('-')
-                f.write(f'   <edge id="{k}i" from="{src}" to="{dst}" priority="78" numLanes="{v["numLanes"]}" speed="{v["maxSpeed"]/3.6}" />\n')
-                f.write(f'   <edge id="{k}o" from="{dst}" to="{src}" priority="46" numLanes="{v["numLanes"]}" speed="{v["maxSpeed"]/3.6}" />\n')
+                f.write(f'   <edge id="{k}" from="{src}" to="{dst}" priority="{v["priority"]}" numLanes="{v["numLanes"]}" speed="{v["maxSpeed"]}" />\n')
             f.write('</edges>')
     
     def generateSumoFile(self, file_name_node, file_name_edge, destiny = "src/instances"):

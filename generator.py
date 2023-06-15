e1098a6f7c0de98b7326594d6dcc4c2d57c44260
Mateus-Gabardo <92892326+Mateus-Gabardo@users.noreply.github.com>
2023-06-15 01:50:02 -0300

    Ajustado instancia do sioux falls

--- generator.py
+++ generator.py
@@ -1,6 +1,8 @@
 from src.data.exemplo.grafo_exemplo_generator import gerar_instancia_exemplo
 from src.data.grid.grafo_grid_generator import gerar_intancia_grid
+from src.data.siouxFalls.generatorSiouxFalls import gerar_intancia_sioux_falls
 
 
 gerar_instancia_exemplo()
 gerar_intancia_grid()
+gerar_intancia_sioux_falls()

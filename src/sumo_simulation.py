d01766998a1605420a5ee248415f42b5e2b6e78c
André E. Zanella <70669215+AndreElliasZanella@users.noreply.github.com>
2023-07-06 23:53:53 -0300

    Update sumo_simulation.py

--- src/sumo_simulation.py
+++ src/sumo_simulation.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import time
 import sys
 import shutil
 from src.generators.sumo_xml_demand_generator import SumoXmlDemandGenerator
@@ -39,9 +40,13 @@ class SumoSimulation:
     def __average_speed(self):
         speedSum = 0.0
         edgeCount = 0
-        for edge in sumolib.xml.parse('src/sumo_data/output.xml', ['edge']):
-            speedSum += float(edge.traveltime)
-            edgeCount += 1
+        try:
+            for edge in sumolib.xml.parse('src/sumo_data/output.xml', ['edge']):
+                speedSum += float(edge.traveltime)
+                edgeCount += 1
+        except Exception:
+            return float("inf")
+        
         avgSpeed = speedSum / edgeCount
         #A velocidade média na borda/faixa dentro do intervalo relatado.
         print('Velocidade média da simulação:', avgSpeed)
@@ -50,7 +55,22 @@ class SumoSimulation:
     def __run_sumo(self):
         output_file = "src/sumo_data/output.xml"
         command = self.sumoCmd + ["--edgedata-output", output_file]
-        subprocess.call(command)
+        #subprocess.call(command)
+
+        try:
+            start_time = time.time()
+            process = subprocess.Popen(command)
+            
+            # Wait for the process to finish or timeout after 5 seconds
+            while process.poll() is None and time.time() - start_time < 5:
+                time.sleep(0.1)
+            
+            if process.poll() is None:
+                process.terminate()  # Cancel the operation if it exceeds 5 seconds
+            
+        except subprocess.CalledProcessError as e:
+            # Handle any error that occurred during the subprocess execution
+            print("Error: ", e)
     
 
 
